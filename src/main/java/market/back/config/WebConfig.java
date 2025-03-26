54b655f20eadefa433387bb239d08fac21b9fbc8
baejeu <zeus20240228@gmail.com>
2025-03-26 17:34:06 +0900

    [2025:03:26] 깃허브 액션 테스트

--- src/main/java/market/back/config/WebConfig.java
+++ src/main/java/market/back/config/WebConfig.java
@@ -1,17 +1,22 @@
 package market.back.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Slf4j
 public class WebConfig implements WebMvcConfigurer {
     @Value("${spring.front.url}")
     private String frontUrl;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+
+        log.info("front url: {}", frontUrl);
+
         registry.addMapping("/**")
                 .allowedOrigins(frontUrl)
                 .allowedMethods("*")
